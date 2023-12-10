245da715971966cc75588c4009d609ea51452832
Filtee <filtee0812@gmail.com>
2023-12-10 13:17:38 +0800

    Updated.

--- code/machine/machine.cc
+++ code/machine/machine.cc
@@ -216,7 +216,7 @@ Machine::WriteRegister(int num, int value) {
     registers[num] = value;
 }
 
-int Machine::findFreeByLU() {
+int Machine::findFreeByLRU() {
     int mi = 0;
     for (int i = 0; i < NumPhysPages; i++) {
         ASSERT(GlobalPageTable[i].RefPageTable != NULL);
